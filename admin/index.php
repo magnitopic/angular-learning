0a54a0824eb02d74bcb5464f612e1db1cc9ea430
magnitopic <magnitopic@gmail.com>
2024-01-08 17:40:14 +0100

    small changes

--- admin/index.php
+++ admin/index.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Admin</title>
 </head>
 <body>
     
     <?php include("menu.html"); ?>
+
+    <h2>Welcome to the store administration!</h2>
 
 </body>
 </html>
